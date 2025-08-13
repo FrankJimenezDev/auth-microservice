d5174e40f984c6a394dd1853b8651f899ed5e2c0
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2025-08-13 02:05:22 -0300

    [Add] Se agrega documentacion de dto usuarios al swagger

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,42 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+
+    @ApiProperty({
+        description: 'The username of the user',
+        example: 'john_doe',
+    })
+    username?: string;
+
+    @ApiProperty({
+        description: 'The password of the user',
+        example: 'strongPassword123',
+    })
+    password?: string;
+
+    @ApiProperty({
+        description: 'The email of the user',
+        example: 'test1@test.com'
+    })
+    email?: string;
+
+    @ApiProperty({
+        description: 'The first name of the user',
+        example: 'John',
+    })
+    firstName?: string;
+
+    @ApiProperty({
+        description: 'The last name of the user',
+        example: 'Doe',
+    })
+    lastName?: string;
+
+    @ApiProperty({
+        description: 'The age of the user',
+        example: 30,
+    })
+    age?: number;
+}
