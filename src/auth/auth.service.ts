acd235eb36840652e8edc87377cd51e09b9f3f45
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2025-08-16 18:13:11 -0300

    [Config] Se configura servicio de auth con metodos validateToken y logout, ademas seteamos la clave valor para redis

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException, Inject } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import Redis from 'ioredis';
+
 @Injectable()
 export class AuthService {
   private readonly logger = new Logger(AuthService.name);
@@ -9,8 +11,11 @@ export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
-  ) { }
-  async logIn(createAuthDto: CreateAuthDto) : Promise<string> {
+    @Inject('REDIS_CLIENT') 
+    private readonly redis: Redis, 
+  ) {}
+
+  async logIn(createAuthDto: CreateAuthDto): Promise<string> {
     try {
       const user = await this.usersService.findOne(createAuthDto.usernameOrEmail);
       const isMatch = await user.comparePassword(createAuthDto.password);
@@ -22,12 +27,33 @@ export class AuthService {
       const payload = { username: user.username, sub: user.id };
       const token = await this.jwtService.signAsync(payload);
 
-      return token;
+      await this.redis.set(`user:${user.id}`, token);
 
+      return token;
     } catch (error) {
-      this.logger.error('Error logginf user:', error);
+      this.logger.error('Error logging user:', error);
       return error.message;
     }
   }
 
+  async validateToken(userId: string, token: string) {
+    try {
+      this.jwtService.verify(token);
+    } catch {
+      throw new UnauthorizedException('Token inválido');
+    }
+
+    const storedToken = await this.redis.get(`user:${userId}`);
+    if (storedToken !== token) {
+      throw new UnauthorizedException(
+        'Tu sesión fue cerrada porque iniciaste sesión en otro dispositivo',
+      );
+    }
+
+    return true;
+  }
+
+  async logout(userId: string) {
+    await this.redis.del(`user:${userId}`);
+  }
 }
