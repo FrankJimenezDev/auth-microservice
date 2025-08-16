5d4c9b1d6494b00e809317f4ee1e435b1afd8ac7
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2025-08-16 18:55:08 -0300

    [Add] Se implementa UseGuards con la clase JwtAuthGuard para manejar validar jwt en el controlador de users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,33 +1,39 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, ClassSerializerInterceptor, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-atuth.guard';
 
 @Controller('users')
 @UseInterceptors(ClassSerializerInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)  
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':username')
   findOne(@Param('username') usernameOrEmail: string) {
     return this.usersService.findOne(usernameOrEmail);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':usernameOrEmail')
   update(@Param('usernameOrEmail') usernameOrEmail: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(usernameOrEmail, updateUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':usernameOrEmail')
   remove(@Param('usernameOrEmail') usernameOrEmail: string) {
     return this.usersService.remove(usernameOrEmail);
