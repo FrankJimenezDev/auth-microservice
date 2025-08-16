2849059bb25412bd63add77e3fe859f26f54b584
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2025-08-16 18:30:22 -0300

    [Config] importamos redismodule en el app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './shared/db/database.module';
+import { RedisModule } from './redis.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), 
+    AuthModule, 
+    UsersModule, 
+    DatabaseModule, 
+    RedisModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
